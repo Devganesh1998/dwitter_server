c2942804534328e944c3ba475b06012b15895f84
devganesh1998 <devganesh91@gmail.com>
2021-05-14 17:36:31 +0530

    added prettier config and auto-fixed

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,7 @@
 export const isDev = process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() === 'development';
 export const isProd = process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() === 'production';
 export const PORT = process.env.PORT || 4500;
-export const ALLOWED_ORIGINS = [process.env.ALLOWED_ORIGIN, isDev && 'http://localhost:3000'].filter(origin => !!origin);
+export const ALLOWED_ORIGINS = [
+	process.env.ALLOWED_ORIGIN,
+	isDev && 'http://localhost:3000',
+].filter((origin) => !!origin);
--- src/index.ts
+++ src/index.ts
@@ -13,42 +13,41 @@ app.use(morgan(isProd ? 'combined' : 'dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/info", (_req, res) => {
-  res.send("Welcome to Dwitter server");
+app.get('/info', (_req, res) => {
+	res.send('Welcome to Dwitter server');
 });
-
 (async () => {
-  if (isDev) {
-    const { default: cors } = await import('cors');
-    app.use(cors());
-  } else {
-    app.use(function (req, res, next) {
-      const origin = req.headers.origin;
-      if (origin && ALLOWED_ORIGINS.indexOf(origin) > -1) {
-        res.setHeader("Access-Control-Allow-Origin", origin);
-      }
-      res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, cookie"
-      );
-      res.header("Access-Control-Allow-Credentials", 'true');
-      next();
-    });
-  }
-  let retries = 5;
-  while (retries) {
-    try {
-      await db.sync();
-      app.listen(PORT, () => {
-        console.log(`listening on: http://localhost:${PORT}`);
-      });
-      break;
-    } catch (err) {
-      console.error(err);
-      retries -= 1;
-      console.log(`Retries left: ${retries}, retrying in 5 seconds.`);
-      // wait 5 seconds
-      await new Promise((res) => setTimeout(res, 5000));
-    }
-  }
+	if (isDev) {
+		const { default: cors } = await import('cors');
+		app.use(cors());
+	} else {
+		app.use(function (req, res, next) {
+			const origin = req.headers.origin;
+			if (origin && ALLOWED_ORIGINS.indexOf(origin) > -1) {
+				res.setHeader('Access-Control-Allow-Origin', origin);
+			}
+			res.header(
+				'Access-Control-Allow-Headers',
+				'Origin, X-Requested-With, Content-Type, Accept, cookie'
+			);
+			res.header('Access-Control-Allow-Credentials', 'true');
+			next();
+		});
+	}
+	let retries = 5;
+	while (retries) {
+		try {
+			await db.sync();
+			app.listen(PORT, () => {
+				console.log(`listening on: http://localhost:${PORT}`);
+			});
+			break;
+		} catch (err) {
+			console.error(err);
+			retries -= 1;
+			console.log(`Retries left: ${retries}, retrying in 5 seconds.`);
+			// wait 5 seconds
+			await new Promise((res) => setTimeout(res, 5000));
+		}
+	}
 })();
