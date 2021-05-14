3bbf68caa26e178577a02d94dd4257afbd98e54c
devganesh1998 <devganesh91@gmail.com>
2021-05-14 17:44:48 +0530

    scripts fix and auto-fix

--- pg-database/config/config.ts
+++ pg-database/config/config.ts
@@ -1,19 +1,22 @@
 type configType = {
-  database: string,
-  url: string,
+	database: string;
+	url: string;
+};
+
+export interface configInterface {
+	development: configType;
+	production: configType;
 }
 
-export interface configInterface { development: configType, production: configType }
-
 const config: configInterface = {
-  development: {
-    database: 'dwitter',
-    url: process.env.DEV_DATABASE_URL || '',
-  },
-  production: {
-    database: 'dwitter',
-    url: process.env.PROD_DATABASE_URL || '',
-  },
+	development: {
+		database: 'dwitter',
+		url: process.env.DEV_DATABASE_URL || '',
+	},
+	production: {
+		database: 'dwitter',
+		url: process.env.PROD_DATABASE_URL || '',
+	},
 };
 
 export default config;
