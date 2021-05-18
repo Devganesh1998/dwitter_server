42c7957a90d565c72b417146ff8511301ecc92b0
devganesh1998 <devganesh91@gmail.com>
2021-05-18 23:20:03 +0530

    added tweet model migration

---
+++ pg-database/migrations/20210518140418-create-tweets.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+module.exports = {
+	// @ts-ignore
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('tweets', {
+      tweetId: {
+				primaryKey: true,
+				allowNull: false,
+				validate: { isUUID: 4 },
+				defaultValue: Sequelize.UUIDV4,
+        type: Sequelize.INTEGER,
+			},
+			tweet: {
+        allowNull: false,
+				type: Sequelize.STRING,
+			},
+			likes: {
+				type: Sequelize.INTEGER,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	// @ts-ignore
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('tweets');
+	},
+};
