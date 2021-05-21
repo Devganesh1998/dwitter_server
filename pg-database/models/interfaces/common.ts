8b7365235e96139abd1f786bd4f95d64e9710e68
devganesh1998 <devganesh91@gmail.com>
2021-05-21 12:51:00 +0000

    common models nodemon config altered

--- pg-database/models/interfaces/common.ts
+++ pg-database/models/interfaces/common.ts
@@ -1,6 +1,8 @@
 import { ModelStatic, Model } from 'sequelize';
+import { TweetAttributes, TweetCreationAttributes } from './Tweet';
+import { UserAttributes, UserCreationAttributes } from './User';
 
 export interface Models {
-	Tweet: ModelStatic<Model>;
-	User: ModelStatic<Model>;
+	Tweet: ModelStatic<Model<TweetAttributes, TweetCreationAttributes>>;
+	User: ModelStatic<Model<UserAttributes, UserCreationAttributes>>;
 }
