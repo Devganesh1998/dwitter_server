897d1d2f6c8b267d95be15283842a041244630c9
devganesh1998 <devganesh91@gmail.com>
2021-05-24 09:56:00 +0000

    added nested comment model

---
+++ pg-database/models/NestedComment.ts
@@ -0,0 +1,49 @@
+import { Sequelize, DataTypes } from 'sequelize';
+import {
+	NestedCommentAttributes,
+	NestedCommentCreationAttributes,
+} from './interfaces/NestedComment';
+import { Models, CustomModel } from './interfaces/common';
+
+export default function CommentModel(
+	sequelize: Sequelize,
+	dataTypes: typeof DataTypes
+): CustomModel<NestedCommentAttributes, NestedCommentCreationAttributes> {
+	const NestedComment: CustomModel<NestedCommentAttributes, NestedCommentCreationAttributes> =
+		sequelize.define(
+			'NestedComment',
+			{
+				nestedCommentId: {
+					primaryKey: true,
+					type: dataTypes.UUID,
+					validate: { isUUID: 4 },
+					defaultValue: dataTypes.UUIDV4,
+				},
+				level: {
+					type: dataTypes.INTEGER,
+					defaultValue: 0,
+				},
+			},
+			{
+				tableName: 'nested_comments',
+				timestamps: true,
+			}
+		);
+
+	NestedComment.associate = (models: Models) => {
+		NestedComment.belongsTo(models.Comment, {
+			foreignKey: {
+				name: 'parentCommentId',
+				allowNull: false,
+			},
+		});
+		NestedComment.belongsTo(models.Comment, {
+			foreignKey: {
+				name: 'childCommentId',
+				allowNull: false,
+			},
+		});
+	};
+
+	return NestedComment;
+}
