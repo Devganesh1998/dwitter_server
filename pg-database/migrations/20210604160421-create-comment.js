d97626cd343a7ecb198d4f68a2b1b087d28c6be8
devganesh1998 <devganesh91@gmail.com>
2021-05-24 10:15:00 +0000

    missed

--- pg-database/migrations/20210604160421-create-comment.js
+++ pg-database/migrations/20210604160421-create-comment.js
@@ -3,11 +3,11 @@ module.exports = {
 	// @ts-ignore
 	up: async (queryInterface, Sequelize) => {
 		await queryInterface.createTable('comments', {
-			id: {
-				allowNull: false,
-				autoIncrement: true,
+			commentId: {
 				primaryKey: true,
-				type: Sequelize.INTEGER,
+				type: Sequelize.UUID,
+				validate: { isUUID: 4 },
+				defaultValue: Sequelize.UUIDV4,
 			},
 			comment: {
 				type: Sequelize.TEXT,
