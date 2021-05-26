d83e3afd84616758888fef0d168ab8df3e755e43
devganesh1998 <devganesh91@gmail.com>
2021-05-26 10:37:00 +0000

    tweet instance added

--- pg-database/models/Tweet.ts
+++ pg-database/models/Tweet.ts
@@ -1,39 +1,40 @@
 import { Sequelize, DataTypes } from 'sequelize';
-import { TweetAttributes, TweetCreationAttributes } from './interfaces/Tweet';
+import { TweetAttributes, TweetCreationAttributes, TweetInstance } from './interfaces/Tweet';
 import { Models, CustomModel } from './interfaces/common';
 
 export default function TweetModel(
 	sequelize: Sequelize,
 	dataTypes: typeof DataTypes
 ): CustomModel<TweetAttributes, TweetCreationAttributes> {
-	const Tweet: CustomModel<TweetAttributes, TweetCreationAttributes> = sequelize.define(
-		'Tweet',
-		{
-			tweetId: {
-				primaryKey: true,
-				type: dataTypes.UUID,
-				validate: { isUUID: 4 },
-				defaultValue: dataTypes.UUIDV4,
-			},
-			tweet: {
-				allowNull: false,
-				type: dataTypes.TEXT({ length: 'medium' }),
-			},
-			likes: {
-				type: dataTypes.BIGINT,
-				defaultValue: 0,
-			},
-		},
-		{
-			tableName: 'tweets',
-			indexes: [
-				{
-					fields: [{ name: 'createdAt', order: 'DESC' }],
+	const Tweet: CustomModel<TweetAttributes, TweetCreationAttributes> =
+		sequelize.define<TweetInstance>(
+			'Tweet',
+			{
+				tweetId: {
+					primaryKey: true,
+					type: dataTypes.UUID,
+					validate: { isUUID: 4 },
+					defaultValue: dataTypes.UUIDV4,
 				},
-			],
-			timestamps: true,
-		}
-	);
+				tweet: {
+					allowNull: false,
+					type: dataTypes.TEXT({ length: 'medium' }),
+				},
+				likes: {
+					type: dataTypes.BIGINT,
+					defaultValue: 0,
+				},
+			},
+			{
+				tableName: 'tweets',
+				indexes: [
+					{
+						fields: [{ name: 'createdAt', order: 'DESC' }],
+					},
+				],
+				timestamps: true,
+			}
+		);
 
 	Tweet.associate = (models: Models) => {
 		Tweet.belongsTo(models.User, {
--- pg-database/models/interfaces/Tweet.ts
+++ pg-database/models/interfaces/Tweet.ts
@@ -1,10 +1,17 @@
-import { Optional } from 'sequelize';
+import { Optional, Model } from 'sequelize';
 
 export interface TweetAttributes {
 	tweetId: string;
 	tweet: string;
 	likes: number;
-	userId: string;
+	userId?: string;
 }
 
 export type TweetCreationAttributes = Optional<TweetAttributes, 'tweetId'>;
+
+export interface TweetInstance
+	extends Model<TweetAttributes, TweetCreationAttributes>,
+		TweetAttributes {
+	createdAt: Date;
+	updatedAt: Date;
+}
