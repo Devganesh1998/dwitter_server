a895f4c9fb767217dfa6e540310e5fab29fcbb07
devganesh1998 <devganesh91@gmail.com>
2021-05-27 13:22:00 +0000

    auth service integrated in auth controller

--- pg-database/models/interfaces/common.ts
+++ pg-database/models/interfaces/common.ts
@@ -1,12 +1,14 @@
-import { ModelStatic, Model, ModelCtor } from 'sequelize';
+import { Model, ModelCtor } from 'sequelize';
 import { CommentAttributes, CommentCreationAttributes } from './Comment';
+import { NestedCommentAttributes, NestedCommentCreationAttributes } from './NestedComment';
 import { TweetAttributes, TweetCreationAttributes } from './Tweet';
 import { UserAttributes, UserCreationAttributes } from './User';
 
 export interface Models {
-	Tweet: ModelStatic<Model<TweetAttributes, TweetCreationAttributes>>;
-	User: ModelStatic<Model<UserAttributes, UserCreationAttributes>>;
-	Comment: ModelStatic<Model<CommentAttributes, CommentCreationAttributes>>;
+	Tweet: ModelCtor<Model<TweetAttributes, TweetCreationAttributes>>;
+	User: ModelCtor<Model<UserAttributes, UserCreationAttributes>>;
+	Comment: ModelCtor<Model<CommentAttributes, CommentCreationAttributes>>;
+	NestedComment: ModelCtor<Model<NestedCommentAttributes, NestedCommentCreationAttributes>>;
 }
 
 export type CustomModel<TModelAttributes = any, TCreationAttributes = any> = ModelCtor<
--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,14 +1,15 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import AuthService from '../services/auth.service';
 
 class AuthController {
-	service: any;
+	private service: typeof AuthService;
 
-	constructor(service: any) {
+	constructor(service: typeof AuthService) {
 		this.service = service;
 	}
 
-	// eslint-disable-next-line class-methods-use-this
-	async login(_req: Request, res: Response) {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars,class-methods-use-this
+	async login(_req: Request, res: Response, _next: NextFunction) {
 		try {
 			res.send({ message: 'Login successfull' });
 		} catch (error) {
@@ -17,10 +18,18 @@ class AuthController {
 		}
 	}
 
-	// eslint-disable-next-line class-methods-use-this
-	async register(_req: Request, res: Response) {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	async register(req: Request, res: Response, _next: NextFunction) {
 		try {
-			res.send({ message: 'Registration successfull' });
+			const userData = req.body;
+			const results = await this.service.createUser({
+				...userData,
+				accountType: 'TRAIL',
+				accountStatus: 'ENABLED',
+				isVerified: false,
+				userType: 'INTERNAL',
+			});
+			res.send({ message: 'Registration successfull', results });
 		} catch (error) {
 			console.error(error);
 			res.status(500).json({ error_msg: 'Internal server error' });
@@ -28,4 +37,4 @@ class AuthController {
 	}
 }
 
-export default new AuthController('authService');
+export default new AuthController(AuthService);
