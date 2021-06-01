20a00bacdbc4b175d829d7b8daaa8ffd062a2dbe
devganesh1998 <devganesh91@gmail.com>
2021-06-01 21:44:31 +0530

    auth service altered

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,25 +3,45 @@ import { UserAttributes } from '../../pg-database/models/interfaces/User';
 import { db, models } from '../../pg-database/models';
 
 export default class AuthService {
-	static async getUserPassFromPhoneNo(phoneNo: number): Promise<Record<string, any>> {
+	static async getUserFromPhoneNo(phoneNo: number): Promise<Record<string, any>> {
 		const [user] =
-			(await db.query('SELECT "password" from users WHERE "phoneNo" = :phoneNo', {
-				replacements: {
-					phoneNo,
-				},
-				type: QueryTypes.SELECT,
-			})) || [];
+			(await db.query(
+				'SELECT "password", "userId", "email", "phoneNo", "isVerified", "userName", "accountStatus", "accountType", "userType" from users WHERE "phoneNo" = :phoneNo',
+				{
+					replacements: {
+						phoneNo,
+					},
+					type: QueryTypes.SELECT,
+				}
+			)) || [];
 		return user;
 	}
 
-	static async getUserPassFromEmail(email: string): Promise<Record<string, any>> {
+	static async getUserFromEmail(email: string): Promise<Record<string, any>> {
 		const [user] =
-			(await db.query('SELECT "password" from users WHERE email = :email', {
-				replacements: {
-					email,
-				},
-				type: QueryTypes.SELECT,
-			})) || [];
+			(await db.query(
+				'SELECT "password", "userId", "email", "phoneNo", "isVerified", "userName", "accountStatus", "accountType", "userType" from users WHERE email = :email',
+				{
+					replacements: {
+						email,
+					},
+					type: QueryTypes.SELECT,
+				}
+			)) || [];
+		return user;
+	}
+
+	static async getUserFromUserName(userName: string): Promise<Record<string, any>> {
+		const [user] =
+			(await db.query(
+				'SELECT "password", "userId", "email", "phoneNo", "isVerified", "userName", "accountStatus", "accountType", "userType" from users WHERE userName = :userName',
+				{
+					replacements: {
+						userName,
+					},
+					type: QueryTypes.SELECT,
+				}
+			)) || [];
 		return user;
 	}
 
