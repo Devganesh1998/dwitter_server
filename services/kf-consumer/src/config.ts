3cdcb3d94439bdcb3764522d5d41f9da03923c8d
devganesh1998 <devganesh91@gmail.com>
2021-06-10 20:18:45 +0530

    consumer_service: redis client added

---
+++ services/kf-consumer/src/config.ts
@@ -0,0 +1,3 @@
+export const SESSION_EXPIRE_IN_MS = 6 * 60 * 60 * 1000; // 6hrs in milliseconds
+
+export const SESSION_EXPIRE_IN_S = 6 * 60 * 60; // 6hrs in seconds
---
+++ services/kf-consumer/src/utils.ts
@@ -0,0 +1,27 @@
+import redis from 'redis';
+import { promisify } from 'util';
+import { CustomRedisClient } from '../types';
+
+const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1';
+const REDIS_PORT: number = parseInt(process.env.REDIS_PORT || '', 10) || 6379;
+
+const getRedisClient = (): CustomRedisClient => {
+    const redisClient: any = redis.createClient({
+        host: REDIS_HOST,
+        port: REDIS_PORT,
+    });
+    redisClient.setAsync = promisify(redisClient.set).bind(redisClient);
+    redisClient.getAsync = promisify(redisClient.get).bind(redisClient);
+    redisClient.llenAsync = promisify(redisClient.llen).bind(redisClient);
+    redisClient.hsetAsync = promisify(redisClient.hset).bind(redisClient);
+    redisClient.hgetAsync = promisify(redisClient.hget).bind(redisClient);
+    redisClient.rpushAsync = promisify(redisClient.rpush).bind(redisClient);
+    redisClient.expireAsync = promisify(redisClient.expire).bind(redisClient);
+    redisClient.hexistsAsync = promisify(redisClient.hexists).bind(redisClient);
+    redisClient.saddAsync = promisify(redisClient.sadd).bind(redisClient);
+    redisClient.sremAsync = promisify(redisClient.srem).bind(redisClient);
+    redisClient.delAsync = promisify(redisClient.del).bind(redisClient);
+    return redisClient;
+};
+
+export default getRedisClient;
