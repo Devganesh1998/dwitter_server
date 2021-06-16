535b1bf53f835b128979fe974759c1edfaad0fad
devganesh1998 <devganesh91@gmail.com>
2021-06-16 20:11:44 +0530

    migrated username availability to user controlle

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -9,6 +9,34 @@ class UserController {
         this.elastic = elasticClient;
     }
 
+    async availability(req: Request, res: Response, _next: NextFunction) {
+        try {
+            const { userName = '' } = req.query;
+            const { statusCode, body: { count = 0 } = {} } = await this.elastic.count({
+                index: 'users',
+                body: {
+                    query: {
+                        term: {
+                            'userName.keyword': {
+                                value: userName,
+                            },
+                        },
+                    },
+                },
+            });
+            if (statusCode !== 200) {
+                return res.status(500).json({ error_msg: 'Internal server error' });
+            }
+            if (count === 0) {
+                return res.send({ userName, isAvailable: true });
+            }
+            return res.send({ userName, isAvailable: false });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error_msg: 'Internal server error' });
+        }
+    }
+
     async autoComplete(req: Request, res: Response, _next: NextFunction) {
         try {
             const { fieldType = '', prefix = '' } = req.body;
