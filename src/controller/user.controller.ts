76daa894e78732bcdb261d0527adcc3c6fadbd55
devganesh1998 <devganesh91@gmail.com>
2021-06-17 20:42:44 +0530

    filter output fields in autocomplete

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import { Client } from '@elastic/elasticsearch';
+import { SuggestionResult } from '../../types';
 import elasticClient from '../utils/getElasticClient';
+import { UserAttributes } from '../../pg-database/models/interfaces/User';
 
 class UserController {
     private elastic: Client;
@@ -40,29 +42,70 @@ class UserController {
     async autoComplete(req: Request, res: Response, _next: NextFunction) {
         try {
             const { fieldType = '', prefix = '' } = req.body;
-            const { statusCode, body: { suggest: { suggestions = [] } = {} } = {} } =
-                await this.elastic.search({
-                    index: 'users',
-                    body: {
-                        suggest: {
-                            suggestions: {
-                                prefix,
-                                completion: {
-                                    field: fieldType,
-                                    fuzzy: {
-                                        fuzziness: 'AUTO',
-                                    },
+            const {
+                statusCode,
+                body: { suggest: { suggestions = [] } = {} } = {},
+            }: {
+                statusCode: number | null;
+                body: { suggest?: { suggestions?: SuggestionResult<UserAttributes>[] } };
+            } = await this.elastic.search({
+                index: 'users',
+                body: {
+                    suggest: {
+                        suggestions: {
+                            prefix,
+                            completion: {
+                                field: fieldType,
+                                fuzzy: {
+                                    fuzziness: 'AUTO',
                                 },
                             },
                         },
                     },
-                });
+                },
+            });
             if (statusCode !== 200) {
                 return res.status(500).json({ error_msg: 'Internal server error' });
             }
             return res.send({
                 suggestions: suggestions.reduce(
-                    (acc: [], { options }: { options: string[] }) => [...acc, ...options],
+                    (acc: Array<{ text: string; doc: UserAttributes }>, { options }) => {
+                        const filteredOptions = options.map(({ text, _source }) => {
+                            const outputKeys = [
+                                'name',
+                                'age',
+                                'gender',
+                                'userName',
+                                'phoneNo',
+                                'email',
+                                'accountStatus',
+                                'accountType',
+                                'userType',
+                                'isVerified',
+                                'followersCount',
+                                'followingCount',
+                                'dateOfBirth',
+                                'countryCode',
+                                'description',
+                                'profileImgUrl',
+                                'posterImgUrl',
+                            ];
+                            const filteredOutputDoc = Object.entries(_source)
+                                .filter(([key]) => outputKeys.includes(key))
+                                .reduce(
+                                    (a, [key, value]) => ({
+                                        ...a,
+                                        [key]: value,
+                                    }),
+                                    {}
+                                ) as unknown as UserAttributes;
+                            return {
+                                text,
+                                doc: filteredOutputDoc,
+                            };
+                        });
+                        return [...acc, ...filteredOptions];
+                    },
                     []
                 ),
             });
