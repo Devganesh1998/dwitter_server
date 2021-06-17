9d05ea1e682d5ffcfcff0f130f85aa1e8cfa9ec6
devganesh1998 <devganesh91@gmail.com>
2021-06-17 21:14:44 +0530

    hashtag attributes and interface

---
+++ pg-database/models/interfaces/Hashtag.ts
@@ -0,0 +1,17 @@
+import { Model } from 'sequelize';
+
+export interface HashTagAttributes {
+    hashtag: string;
+    description: string;
+    followersCount?: number;
+    createdBy?: string;
+}
+
+export type HashTagCreationAttributes = Record<string, unknown>;
+
+export interface HashTagInstance
+    extends Model<HashTagAttributes, HashTagCreationAttributes>,
+        HashTagAttributes {
+    createdAt: Date;
+    updatedAt: Date;
+}
