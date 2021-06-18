258bf9cfa587ac0016999cdb4afb084e849ef59c
devganesh1998 <devganesh91@gmail.com>
2021-06-18 20:23:44 +0530

    tweet-hashtag & hashtag migrations altered

---
+++ pg-database/migrations/20210706164315-create-hashtag.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+module.exports = {
+    // @ts-ignore
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('hashtags', {
+            hashtag: {
+                primaryKey: true,
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            description: {
+                type: Sequelize.TEXT,
+            },
+            followersCount: {
+                type: Sequelize.BIGINT,
+                defaultValue: 0,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    // @ts-ignore
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('hashtags');
+    },
+};
