ac7a5ad13b0b59a90ea5077823fa4bab3be489fd
devganesh1998 <devganesh91@gmail.com>
2021-06-20 12:11:34 +0530

    tweets routes added

---
+++ src/routes/tweet.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { ControllerArgs } from '../../types';
+
+const router = Router();
+
+router.post('/', (...args: ControllerArgs) => TweetController.create(...args));
+router
+    .route('/:id')
+    .get((...args: ControllerArgs) => TweetController.get(...args))
+    .put((...args: ControllerArgs) => TweetController.update(...args))
+    .delete((...args: ControllerArgs) => TweetController.delete(...args));
+
+export default router;
