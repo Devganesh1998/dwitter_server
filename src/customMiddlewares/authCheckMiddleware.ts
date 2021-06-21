3a06086f4011584df94a86e211f0a852221462e9
devganesh1998 <devganesh91@gmail.com>
2021-06-21 19:24:34 +0530

    authCheck middleware added

---
+++ src/customMiddlewares/authCheckMiddleware.ts
@@ -0,0 +1,56 @@
+import { NextFunction, Request, Response } from 'express';
+import getRedisClient from '../../redis-cache';
+
+const redisClient = getRedisClient();
+
+const autoSessionRefresh = async (
+    req: Request & {
+        user?: {
+            userId: string;
+            email: string;
+            phoneNo: string;
+            userName: string;
+            accountStatus: string;
+            accountType: string;
+            userType: string;
+            isVerified: boolean;
+        };
+    },
+    res: Response,
+    next: NextFunction
+): Promise<Response<any, Record<string, any>> | undefined> => {
+    try {
+        const { at: hashedSessionId } = req.cookies || {};
+        const [userId, email, phoneNo, userName, accountStatus, accountType, userType, isVerified] =
+            await redisClient.hmgetAsync([
+                `session:${hashedSessionId}`,
+                'userId',
+                'email',
+                'phoneNo',
+                'userName',
+                'accountStatus',
+                'accountType',
+                'userType',
+                'isVerified',
+            ]);
+        if (!userId) {
+            return res.sendStatus(401);
+        }
+        req.user = {
+            userId,
+            email,
+            phoneNo,
+            userName,
+            accountStatus,
+            accountType,
+            userType,
+            isVerified: isVerified === 'true',
+        };
+        next();
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error_msg: 'Internal server error' });
+    }
+};
+
+export default autoSessionRefresh;
