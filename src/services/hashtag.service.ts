06d92a7412c987ce31cfc427228396e84f16ae4e
devganesh1998 <devganesh91@gmail.com>
2021-06-21 20:34:41 +0530

    hashtag service getHashtags

--- src/services/hashtag.service.ts
+++ src/services/hashtag.service.ts
@@ -1,7 +1,23 @@
+import { QueryTypes } from 'sequelize';
 import { HashTagAttributes } from '../../pg-database/models/interfaces/Hashtag';
-import { models } from '../../pg-database/models';
+import { models, db } from '../../pg-database/models';
 
 export default class AuthService {
+    static async getHashtags(hashtags: string[]): Promise<Record<string, any>> {
+        const result =
+            (await db.query(
+                'SELECT "hashtag" from hashtags WHERE "hashtag" IN (:hashtags) LIMIT :limit',
+                {
+                    replacements: {
+                        hashtags: hashtags.join("','"),
+                        limit: hashtags.length,
+                    },
+                    type: QueryTypes.SELECT,
+                }
+            )) || [];
+        return result;
+    }
+
     static async createHashTag({
         hashtag,
         category,
