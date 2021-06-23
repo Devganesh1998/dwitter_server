41e22712db14355a7577e58f24d0bb16686c0f35
devganesh1998 <devganesh91@gmail.com>
2021-06-23 20:24:45 +0530

    tweetUserTag bulk support

--- src/services/tweetUserTag.service.ts
+++ src/services/tweetUserTag.service.ts
@@ -18,4 +18,28 @@ export default class TweetUserService {
         const result = cursor.toJSON() as TweetUserTagAttributes;
         return result;
     }
+
+    static async associateTweetUser__bulk(
+        tweetUserTags: Array<
+            Omit<TweetUserTagAttributes, 'tweetUsertagId'> & {
+                tweetId: string;
+                userName: string;
+            }
+        >
+    ): Promise<TweetUserTagAttributes[]> {
+        const userIds = await UserService.getUserIdsFromUsernames(
+            ...tweetUserTags.map(({ userName }) => userName)
+        );
+        const cursor = await models.TweetUserTag.bulkCreate(
+            tweetUserTags.map(({ userName, ...doc }, index) => {
+                return {
+                    ...doc,
+                    userId: userIds[index]?.userId,
+                };
+            }),
+            { validate: true }
+        );
+        const result = cursor.map((doc) => doc.toJSON()) as TweetUserTagAttributes[];
+        return result;
+    }
 }
