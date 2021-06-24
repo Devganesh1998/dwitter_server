4489934ad70547eb39d5ac2bffb8728e2eecb286
devganesh1998 <devganesh91@gmail.com>
2021-06-24 19:15:45 +0530

    userName also returned in fetch

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,10 +4,10 @@ import { db } from '../../pg-database/models';
 export default class UserService {
     static async getUserIdsFromUsernames(
         ...userNames: string[]
-    ): Promise<Array<{ userId: string }>> {
+    ): Promise<Array<{ userId: string; userName: string }>> {
         const result =
             ((await db.query(
-                'SELECT "userId" from users WHERE "userName" IN (:userNames) LIMIT :limit',
+                'SELECT "userId", "userName" from users WHERE "userName" IN (:userNames) LIMIT :limit',
                 {
                     replacements: {
                         userNames,
@@ -15,7 +15,7 @@ export default class UserService {
                     },
                     type: QueryTypes.SELECT,
                 }
-            )) as Array<{ userId: string }>) || [];
+            )) as Array<{ userId: string; userName: string }>) || [];
         return result;
     }
 }
