c201fdae978a768d4b14a08ace621d34891f1632
devganesh1998 <devganesh91@gmail.com>
2021-06-24 20:11:45 +0530

    req body validation for tweet creation

--- src/routes/tweet.routes.ts
+++ src/routes/tweet.routes.ts
@@ -1,13 +1,41 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { AuthenticatedControllerArgs } from '../../types';
 import TweetController from '../controller/tweet.controller';
 import authCheckMiddleware from '../customMiddlewares/authCheckMiddleware';
+import verifyValidations from '../customMiddlewares/verifyValidations';
 import autoSessionRefresh from '../customMiddlewares/autoSessionRefresh';
 
 const router = Router();
 
-router.post('/', authCheckMiddleware, autoSessionRefresh, (...args: AuthenticatedControllerArgs) =>
-    TweetController.create(...args)
+router.post(
+    '/',
+    authCheckMiddleware,
+    [
+        body('tweet', 'tweet field is required and should be type as string')
+            .exists({ checkFalsy: true })
+            .bail()
+            .isString()
+            .bail()
+            .trim(),
+        body(
+            'hashtags',
+            'hashtags field is required and should be type as array of strings, but can be empty array'
+        )
+            .exists()
+            .bail()
+            .isArray(),
+        body(
+            'userTags',
+            'userTags field is required and should be type as array of strings, but can be empty array'
+        )
+            .exists()
+            .bail()
+            .isArray(),
+    ],
+    verifyValidations,
+    autoSessionRefresh,
+    (...args: AuthenticatedControllerArgs) => TweetController.create(...args)
 );
 // router
 //     .route('/:id')
