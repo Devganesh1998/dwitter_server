beec3009525a079c36c6494a9303a6da66ba2887
devganesh1998 <devganesh91@gmail.com>
2021-06-26 11:12:45 +0530

    createdBy field added to tweet getBy Id

--- src/services/tweet.service.ts
+++ src/services/tweet.service.ts
@@ -1,5 +1,6 @@
+import { QueryTypes } from 'sequelize';
 import { TweetAttributes } from '../../pg-database/models/interfaces/Tweet';
-import { models } from '../../pg-database/models';
+import { models, db } from '../../pg-database/models';
 
 export default class TweetService {
     static async createTweet({
@@ -16,11 +17,20 @@ export default class TweetService {
         return result;
     }
 
-    static async findOneById(tweetId: string): Promise<TweetAttributes | boolean> {
-        const cursor = await models.Tweet.findByPk(tweetId);
-        if (cursor) {
-            const result = cursor.toJSON() as unknown as TweetAttributes;
-            return result;
+    static async findOneById(
+        tweetId: string
+    ): Promise<(TweetAttributes & { createdBy: string }) | boolean> {
+        const [tweetData] = (await db.query(
+            'SELECT t."tweetId", t."tweet", t."likes", t."createdAt", t."updatedAt", u."userName" as "createdBy" from "tweets" as t JOIN "users" as u ON u."userId" = t."userId" WHERE "tweetId" = :tweetId',
+            {
+                replacements: {
+                    tweetId,
+                },
+                type: QueryTypes.SELECT,
+            }
+        )) as Array<TweetAttributes & { createdBy: string }>;
+        if (tweetData) {
+            return tweetData;
         }
         return false;
     }
