e098daebfbd56548ebd7077455c22a8c3d5f0d66
devganesh1998 <devganesh91@gmail.com>
2021-06-27 12:37:34 +0530

    query & body validations added for updateById

--- src/routes/tweet.routes.ts
+++ src/routes/tweet.routes.ts
@@ -58,7 +58,45 @@ router
         autoSessionRefresh,
         (...args: AuthenticatedControllerArgs) => TweetController.findOne(...args)
     )
-    .put((...args: AuthenticatedControllerArgs) => TweetController.updateOne(...args));
+    .put(
+        authCheckMiddleware,
+        [
+            param(
+                'tweetId',
+                'Please provide tweetId as param, it is required and should be in UUID format'
+            )
+                .exists({ checkFalsy: true })
+                .bail()
+                .isUUID()
+                .bail()
+                .trim()
+                .exists({ checkFalsy: true }),
+            body('tweet', 'tweet field is required and should be type as string')
+                .exists({ checkFalsy: true })
+                .bail()
+                .isString()
+                .bail()
+                .trim()
+                .exists({ checkFalsy: true }),
+            body(
+                'hashtags',
+                'hashtags field is required and should be type as array of strings, but can be empty array'
+            )
+                .exists()
+                .bail()
+                .isArray(),
+            body(
+                'userTags',
+                'userTags field is required and should be type as array of strings, but can be empty array'
+            )
+                .exists()
+                .bail()
+                .isArray(),
+        ],
+        verifyValidations,
+        autoSessionRefresh,
+        (...args: AuthenticatedControllerArgs) => TweetController.updateOne(...args)
+    );
 //     .delete((...args: ControllerArgs) => TweetController.delete(...args));
 
 export default router;
