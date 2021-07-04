5dafa4dd05308063d2578a1c6220420659be1986
devganesh1998 <devganesh91@gmail.com>
2021-07-04 19:45:23 +0530

    hashtag routes added to root

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import AuthRoutes from './auth.routes';
 import UserRoutes from './user.routes';
 import TweetRoutes from './tweet.routes';
+import HashtagRoutes from './hashtag.routes';
 
 const router = Router();
 
 router.use('/auth', AuthRoutes);
 router.use('/user', UserRoutes);
 router.use('/tweet', TweetRoutes);
+router.use('/hashtag', HashtagRoutes);
 
 export default router;
