58f9be1dd7ad958f1b49b931a699a5ad5e01d749
devganesh1998 <devganesh91@gmail.com>
2021-07-04 19:54:23 +0530

    hashtag routes validations added

--- src/routes/hashtag.routes.ts
+++ src/routes/hashtag.routes.ts
@@ -1,9 +1,34 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { AuthenticatedControllerArgs } from '../../types';
 import HashtagController from '../controller/hashtag.controller';
+import authCheckMiddleware from '../customMiddlewares/authCheckMiddleware';
+import verifyValidations from '../customMiddlewares/verifyValidations';
+import autoSessionRefresh from '../customMiddlewares/autoSessionRefresh';
 
 const router = Router();
 
-router.post('/', (...args: AuthenticatedControllerArgs) => HashtagController.createOne(...args));
+router.post(
+    '/',
+    authCheckMiddleware,
+    [
+        body('hashtag', 'hashtag field is required and should be type as string')
+            .exists({ checkFalsy: true })
+            .bail()
+            .isString()
+            .bail()
+            .trim()
+            .exists({ checkFalsy: true }),
+        body('category', 'category should be in type string').optional().isString().bail().trim(),
+        body('description', 'description should be in type string')
+            .optional()
+            .isString()
+            .bail()
+            .trim(),
+    ],
+    verifyValidations,
+    autoSessionRefresh,
+    (...args: AuthenticatedControllerArgs) => HashtagController.createOne(...args)
+);
 
 export default router;
